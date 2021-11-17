ba20c9cb8f4074e6cb0ed4c81e5e70079f469687
Prince Felix <princeafelix@gmail.com>
2021-11-17 10:53:43 -0500

    Course Labs Added

---
+++ Lab1/Lab1.cpp
@@ -0,0 +1,15 @@
+﻿// Lab1.cpp : Defines the entry point for the application.
+//
+
+#include "Lab1.h"
+
+#define usingNamespaces 0
+
+using namespace std;
+
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
